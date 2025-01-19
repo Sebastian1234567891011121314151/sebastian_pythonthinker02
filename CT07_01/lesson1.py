c3875fa77f8ac70458804481b5b01c223e4de655
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-01-19 03:34:16 +0000

    Jan 19, 2025, 3:34 AM

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -7,3 +7,10 @@ print("Hello from lesson 1")
 # elif 
 # thig is glass throw in glass bin
 
+# if 
+# thing is plastic throw in plastic bin 
+# elif 
+# thing is paper throw in paper bin 
+# elif 
+# thig is glass throw in glass bin
+
