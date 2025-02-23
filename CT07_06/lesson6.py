5faa71d4e4fba1c2c49c33e78fdd380d4fc56969
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-02-23 03:56:11 +0000

    Feb 23, 2025, 3:56 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,3 +1,17 @@
-students[
-    []
-]
+students=[
+    [seby, 13, sec1, dumb, math]
+    [oliver, 25, working,reading,chinese]
+    [fklasd, 15, sec3,sports,english]]
+for i in students:
+    name, age, grade *other=students
+    print(name+age)
+
+    
+
+
+
+
+
+
+
+
