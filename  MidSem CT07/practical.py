d2976732d1f49b182b77ae2b093566aa8d91186d
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-03-09 03:46:54 +0000

    Mar 9, 2025, 3:46 AM

---  MidSem CT07/practical.py
+++  MidSem CT07/practical.py
@@ -4,7 +4,7 @@ import time
 import random
 print("Tim has "+ str(health) +" health")
 
-while health > -14:
+while health > x:
     health = health - random. randint(1, 15)
     print("Tim has " + str(health) + " left after exploring a dungeon")
     time. sleep(1)
