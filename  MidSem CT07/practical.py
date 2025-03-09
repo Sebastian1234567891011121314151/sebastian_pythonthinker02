f186fbc0f67bee6c20e6e80b5e648ebbc129cc80
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-03-09 03:54:20 +0000

    Mar 9, 2025, 3:54 AM

---  MidSem CT07/practical.py
+++  MidSem CT07/practical.py
@@ -1,13 +1,13 @@
-health=100
-import time
-import random
-print("Tim has "+ str(health) +" health")
+# health=100
+# import time
+# import random
+# print("Tim has "+ str(health) +" health")
 
-while health > 0:
-    health = health - random. randint(1, 15)
-    print("Tim has " + str(health) + " left after exploring a dungeon")
-    time. sleep(1)
-print("Tim died :(")
+# while health > 0:
+#     health = health - random. randint(1, 15)
+#     print("Tim has " + str(health) + " left after exploring a dungeon")
+#     time. sleep(1)
+# print("Tim died :(")
 
 
 list=[]
