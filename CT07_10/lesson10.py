9f527fcb3cfb093bd192d65f0bb8a15e7859cf66
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-04-13 03:07:52 +0000

    Apr 13, 2025, 3:07 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,2 +1,10 @@
-print("Hello from lesson 9")
+window=turtle.Screen()
+window.setup(width=600,height=400)
+t=turtle.Turtle()
+t.shape("turtle")
+t.fillcolor("orange")
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+window.mainloop()
 
