b37d7ff9e873d34620f764fd89d39f658f60c267
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-04-13 04:04:39 +0000

    Apr 13, 2025, 4:04 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -11,17 +11,18 @@ print("Hello from lesson 1")
 #     t.right(90)
 # window.mainloop()
 
-def isElderly(age):
-    if (age>65):
-        return 1 
-    return 0 
+# def isElderly(age):
+#     if (age>65):
+#         return 1 
+#     return 0 
 
-age=input("What is your age")
-if isElderly(int(age)):
-    print("You are eligible for an elderly discount.")
-else:
-    print("You are not eligible for an elderly discount")
+# age=input("What is your age")
+# if isElderly(int(age)):
+#     print("You are eligible for an elderly discount.")
+# else:
+#     print("You are not eligible for an elderly discount")
 
 
 import random 
 x=random.randint(1,100)
+print (x)
