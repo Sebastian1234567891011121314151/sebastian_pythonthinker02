cc3e2250454a38227df525f689a739760b008c52
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2025-04-27 03:17:51 +0000

    Apr 27, 2025, 3:17 AM

--- CT07_11_12_13/lesson11_12_13.py
+++ CT07_11_12_13/lesson11_12_13.py
@@ -2,4 +2,5 @@ print("Hello from lesson 11_12_13")
 
 def get_player_move (board):
     move_input=input("Pick a number from 1 - 9")
-    move=int(move_imput)-
+    move=int(move_imput)-1
+    row+move
